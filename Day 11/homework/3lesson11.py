c51d5a7c35ad093e41bcacf554b4ca9fe7f52cb3
Giorgi1223 <gggiorgi35@gmail.com>
2024-03-11 02:00:05 +0400

    homework

---
+++ Day 11/homework/3lesson11.py
@@ -0,0 +1,7 @@
+#3)შექმენით სია და მომხმარებელს აარჩევინეთ სასურველი მნიშვნელობა
+
+dog=["Spitz", "cocker spaniel", "dalmatian","german shepherd"]
+
+choise = int(input("enter a  num:" ))
+
+print(dog[choise])
