7e536d64e55eab66b42a9e8d205025bef61c7b7e
Giorgi1223 <gggiorgi35@gmail.com>
2024-10-26 00:55:20 +0400

    commit

--- Day 75/classwork/script.js
+++ Day 75/classwork/script.js
@@ -12,3 +12,41 @@ class Rectangle {
 
 let smth = new Rectangle(200, 100)
 console.log (smth.Perimeter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Animal{
+    constructor(name, ){
+        this.name = name
+    }
+
+}
+
+class childmamal {
+    constructor(name, age) {
+        this.name = name
+        super(name, age)
+        this.age = age
+    }
+}
+
+class dog{
+    constructor(name, age, bark) {
+        this.name = name
+        super(name, age)
+        this.bark = bark
+    }
+}
+
+const dog = new Dog("Miki", "brown", "Labrador");
